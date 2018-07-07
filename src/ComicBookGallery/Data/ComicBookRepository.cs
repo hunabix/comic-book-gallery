dd15205f0226453f3ca0d0e95bda04bde90250cc
Alejandro Sobrevilla <hunabix@gmail.com>
2018-07-07 01:52:35 -0500

    Validating a Bug Fix with the Debugger

---
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,75 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>The story was controversial, concluding with the death of Parker in Octopus' body, and Octopus surviving as the now Superior Spider-Man. <strong>Dying Wish</strong> encompassed some of the best selling comics of 2012, with issue #700 listed as the 4th best selling comic of the year.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Writer", Role = "Dan Slott" },
+                    new Artist() { Name = "Penciller", Role = "Humberto Ramos" },
+                    new Artist() { Name = "Inker", Role = "Victor Olazaba" },
+                    new Artist() { Name = "Letterer", Role = "Chris Eliopoulos" },
+                    new Artist() { Name = "Colorists", Role = "Edgar Delgado" },
+                    new Artist() { Name = "Editor", Role = "Stephen Wacker" }
+                }
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                },
+                Favorite = false
+            },
+            new ComicBook()
+            {
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Jeff Smith", Role = "Script" },
+                    new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+                    new Artist() { Name = "Jeff Smith", Role = "Inks" },
+                    new Artist() { Name = "Jeff Smith", Role = "Letters" }
+                },
+                Favorite = false
+            }
+    };
+
+
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+                    break;
+                }                   
+            }
+            return comicBookToReturn;
+        }
+    }
+}
